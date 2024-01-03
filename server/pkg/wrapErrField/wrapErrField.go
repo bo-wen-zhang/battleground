c16fbc2401279a28e7aa345c60450367a9e9d551
bo-wen-zhang <bowenedmondzhang@gmail.com>
2024-01-03 00:19:32 +0000

    Added more tests and WrapFields function.

--- server/pkg/wrapErrField/wrapErrField.go
+++ server/pkg/wrapErrField/wrapErrField.go
@@ -1,44 +1,58 @@
+// I need to add comments to this package :)
 package wrapErrField
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// Extends the wrapError to transport log key-value pairs
-type wrapErrField struct {
+// Enables transporting key-value pairs up a call stack for logging purposes.
+type wrapError struct {
 	msg    string
 	err    error
 	fields []interface{}
 }
 
-func (e wrapErrField) Error() string {
+func (e wrapError) Error() string {
 	return e.msg
 }
 
-func (e wrapErrField) Unwrap() error {
-	return e.err
-}
-
-func Err(reason string, err error, fields []interface{}) error {
-	return &wrapErrField{
-		msg:    fmt.Sprintf("%s: %v", reason, err),
-		err:    err,
-		fields: fields,
+func Unwrap(err error) error {
+	if prev, ok := err.(*wrapError); ok {
+		return prev.err
 	}
+	return errors.Unwrap(err)
 }
 
-// Unwraps the fields from the outer most to the inner most
+// Fields are unwrapped from the top of the stack to the bottom.
 func Fields(err error) []interface{} {
-	prev, ok := err.(*wrapErrField)
+	prev, ok := err.(*wrapError)
 	if !ok {
 		return []interface{}{}
 	}
 	fields := prev.fields
 	for {
-		next, ok := prev.err.(*wrapErrField)
+		next, ok := prev.err.(*wrapError)
 		if !ok {
 			return fields
 		}
-		fields = append(append([]interface{}{}, next.fields...), fields...)
+		fields = append(fields, next.fields...)
 		prev = next
 	}
+}
 
+func WrapFields(err error, fields []interface{}) error {
+	return &wrapError{
+		msg:    err.Error(),
+		err:    err,
+		fields: fields,
+	}
+}
+
+func WrapMsgAndFields(msg string, err error, fields []interface{}) error {
+	return &wrapError{
+		msg:    fmt.Sprintf("%s: %v", msg, err),
+		err:    err,
+		fields: fields,
+	}
 }
--- server/pkg/wrapErrField/wrapErrField_test.go
+++ server/pkg/wrapErrField/wrapErrField_test.go
@@ -2,44 +2,27 @@ package wrapErrField
 
 import (
 	"errors"
-	"fmt"
 	"strings"
 	"testing"
 
+	// zerolog supports including key-value pairs in logs using Fields()
 	"github.com/rs/zerolog"
 )
 
 var ErrSomeError = errors.New("some error")
 
-func TestSingleStrField(t *testing.T) {
+func TestManyFields(t *testing.T) {
 	b := strings.Builder{}
 	logger := zerolog.New(&b)
-	expected := "{\"level\":\"info\",\"name\":\"John\"}\n"
+	expected := "{\"level\":\"info\",\"name\":\"John\",\"age\":23,\"gender\":\"M\"}\n"
 
 	err := func() error {
 		fields := []interface{}{
 			"name", "John",
-		}
-		return Err("some message", ErrSomeError, fields)
-	}()
-
-	logger.Info().Fields(Fields(err)).Send()
-
-	if b.String() != expected {
-		t.Errorf("Logged %s, expected %s", b.String(), expected)
-	}
-}
-
-func TestSingleIntField(t *testing.T) {
-	b := strings.Builder{}
-	logger := zerolog.New(&b)
-	expected := "{\"level\":\"info\",\"age\":23}\n"
-
-	err := func() error {
-		fields := []interface{}{
 			"age", 23,
+			"gender", "M",
 		}
-		return Err("some message", ErrSomeError, fields)
+		return WrapMsgAndFields("some message", ErrSomeError, fields)
 	}()
 
 	logger.Info().Fields(Fields(err)).Send()
@@ -58,7 +41,7 @@ func TestMultipleFields(t *testing.T) {
 		fields := []interface{}{
 			"name", "John", "age", 23,
 		}
-		return Err("some message", ErrSomeError, fields)
+		return WrapMsgAndFields("some message", ErrSomeError, fields)
 	}()
 
 	logger.Info().Fields(Fields(err)).Send()
@@ -71,17 +54,51 @@ func TestMultipleFields(t *testing.T) {
 func TestCallStackHeightTwo(t *testing.T) {
 	b := strings.Builder{}
 	logger := zerolog.New(&b)
-	expected := "{\"level\":\"info\",\"name\":\"John\",\"age\":23}\n"
+	expected := "{\"level\":\"info\",\"age\":23,\"name\":\"John\"}\n"
 
 	err := func() error {
-		fields := []interface{}{"name", "John"}
-		return Err("some message", ErrSomeError, fields)
+		return WrapMsgAndFields("some message", ErrSomeError, []interface{}{"name", "John"})
+	}()
+	newErr := WrapMsgAndFields("some message", err, []interface{}{"age", 23})
+
+	logger.Info().Fields(Fields(newErr)).Send()
+
+	if b.String() != expected {
+		t.Errorf("Logged %s, expected %s", b.String(), expected)
+	}
+}
+
+func TestUnwrap(t *testing.T) {
+	b := strings.Builder{}
+	logger := zerolog.New(&b)
+	expected := "{\"level\":\"info\",\"name\":\"John\"}\n"
+
+	err := func() error {
+		return WrapMsgAndFields("some message", ErrSomeError, []interface{}{"name", "John"})
+	}()
+	newErr := WrapMsgAndFields("some message", err, []interface{}{"age", 23})
+
+	newErr = Unwrap(newErr)
+	logger.Info().Fields(Fields(newErr)).Send()
+
+	if b.String() != expected {
+		t.Errorf("Logged %s, expected %s", b.String(), expected)
+	}
+}
+
+func TestPropogate(t *testing.T) {
+	b := strings.Builder{}
+	logger := zerolog.New(&b)
+	expected := "{\"level\":\"info\",\"name\":\"John\"}\n"
+
+	err := func() error {
+		err := WrapFields(func() error {
+			return WrapMsgAndFields("some message", ErrSomeError, []interface{}{"name", "John"})
+		}(), []interface{}{})
+		return err
 	}()
 
-	newFields := []interface{}{"age", 23}
-	newErr := Err("some message", err, newFields)
-	fmt.Println(err, newErr)
-	logger.Info().Fields(Fields(newErr)).Send()
+	logger.Info().Fields(Fields(err)).Send()
 
 	if b.String() != expected {
 		t.Errorf("Logged %s, expected %s", b.String(), expected)
